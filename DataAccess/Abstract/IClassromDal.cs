ee5b88b0cbd9cecae7a03c3edc9537c0978ed307
acarbey02400 <acarwoody01@gmail.com>
2023-04-07 15:12:36 +0300

    DataAccess katmanında sınıflar eklendi

---
+++ DataAccess/Abstract/IClassromDal.cs
@@ -0,0 +1,16 @@
+﻿using Core.DataAccess.EntityFramework.Abstract;
+using Core.DataAccess.EntityFramework.Concrete;
+using DataAccess.Context;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    public interface IClassromDal:IEntityRepository<Classrom>
+    {
+    }
+}
---
+++ DataAccess/Context/SauDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Context
+{
+    public class SauDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=SauDbContext;Trusted_Connection=true");
+        }
+
+        public DbSet<Classrom> Classroms { get; set; }
+        public DbSet<Degree> Degrees { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Exam> Exams { get; set; }
+        public DbSet<Facultiy> Faculties { get; set; }
+        public DbSet<Lesson> Lessons { get; set; }
+        public DbSet<LessonToStudent> LessonToStudents { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Semester> Semesters { get; set; }
+        public DbSet<Session> Sessions { get; set; }
+        public DbSet<SessionToStudent> SessionToStudents { get; set; }
+        public DbSet<Staff> Staffs { get; set; }
+        public DbSet<Student> Studens { get; set; }
+        public DbSet<TeachingOfType> TeachingOfTypes { get; set; }
+        public DbSet<TypeOfExam> TypeOfExams { get; set; }
+        public DbSet<Year> Years { get; set; }
+
+    }
+}
